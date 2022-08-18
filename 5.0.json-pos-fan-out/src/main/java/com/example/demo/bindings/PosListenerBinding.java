0dacdeff4e15ccb62d35c5f15ae2d2cadd5e9c3b
Prateek <54174687+javaHelper@users.noreply.github.com>
2022-08-19 01:50:58 +0530

    Processing JSON Message Stream
    Processing JSON Message Stream

---
+++ 5.0.json-pos-fan-out/src/main/java/com/example/demo/bindings/PosListenerBinding.java
@@ -0,0 +1,23 @@
+package com.example.demo.bindings;
+
+import com.example.demo.model.HadoopRecord;
+import com.example.demo.model.Notification;
+import com.example.demo.model.PosInvoice;
+import org.apache.kafka.streams.kstream.KStream;
+import org.springframework.cloud.stream.annotation.Input;
+import org.springframework.cloud.stream.annotation.Output;
+
+public interface PosListenerBinding {
+
+    @Input("notification-input-channel")
+    KStream<String, PosInvoice> notificationInputStream();
+
+    @Output("notification-output-channel")
+    KStream<String, Notification> notificationOutputStream();
+
+    @Input("hadoop-input-channel")
+    KStream<String, PosInvoice> hadoopInputStream();
+
+    @Output("hadoop-output-channel")
+    KStream<String, HadoopRecord> hadoopOutputStream();
+}
