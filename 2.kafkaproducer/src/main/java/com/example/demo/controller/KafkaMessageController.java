5c87c3d5cfb719cdbac5ab03a4d708feb778196d
Prateek <54174687+javaHelper@users.noreply.github.com>
2022-08-19 00:19:09 +0530

    Kafka Producer using REST API

---
+++ 2.kafkaproducer/src/main/java/com/example/demo/controller/KafkaMessageController.java
@@ -0,0 +1,23 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.IncomingMessage;
+import com.example.demo.service.KafkaProducerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class KafkaMessageController {
+
+    @Autowired
+    private KafkaProducerService kafkaProducerService;
+
+    @PostMapping("/post")
+    public String sendMessageToKafka(@RequestBody IncomingMessage message) {
+
+        kafkaProducerService.sendMessage(message.getTopic(),message.getKey(), message.getValue());
+        return String.format("Success - Message for key %s is sent to Kafka Topic: %s",
+                message.getKey(), message.getTopic());
+    }
+}
