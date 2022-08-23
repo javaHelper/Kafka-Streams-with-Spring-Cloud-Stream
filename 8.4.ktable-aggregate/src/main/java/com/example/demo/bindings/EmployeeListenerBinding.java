eb5a6829d6aaab65e45e7ccbf3baf10650210f73
Prateek <54174687+javaHelper@users.noreply.github.com>
2022-08-24 01:20:26 +0530

    KTable Aggregation
    KTable Aggregation

---
+++ 8.4.ktable-aggregate/src/main/java/com/example/demo/bindings/EmployeeListenerBinding.java
@@ -0,0 +1,12 @@
+package com.example.demo.bindings;
+
+import com.example.demo.model.Employee;
+import org.apache.kafka.streams.kstream.KStream;
+import org.springframework.cloud.stream.annotation.Input;
+
+public interface EmployeeListenerBinding {
+
+    @Input("employee-input-channel")
+    KStream<String, Employee> employeeInputStream();
+
+}
