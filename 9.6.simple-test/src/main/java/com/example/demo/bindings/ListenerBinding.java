8c6755ac7bb6b6b5e323f66be4a59ce25e21dc7d
Prateek <54174687+javaHelper@users.noreply.github.com>
2022-08-28 19:12:51 +0530

    Stream Listener Manual Testing
    Stream Listener Manual Testing

---
+++ 9.6.simple-test/src/main/java/com/example/demo/bindings/ListenerBinding.java
@@ -0,0 +1,14 @@
+package com.example.demo.bindings;
+
+import org.apache.kafka.streams.kstream.KStream;
+import org.springframework.cloud.stream.annotation.Input;
+import org.springframework.cloud.stream.annotation.Output;
+
+public interface ListenerBinding {
+
+    @Input("process-in-0")
+    KStream<String, String> inputStream();
+
+    @Output("process-out-0")
+    KStream<String, String> outStream();
+}
