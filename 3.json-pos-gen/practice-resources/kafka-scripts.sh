41d8f9b2e38592d1ffb7720bce93b997f03e1a08
Prateek <54174687+javaHelper@users.noreply.github.com>
2022-10-18 15:49:53 +0530

    json-pos-gen

--- 3.json-pos-gen/practice-resources/kafka-scripts.sh
+++ 3.json-pos-gen/practice-resources/kafka-scripts.sh
@@ -1,12 +1,18 @@
 
-
+# Below Line only for windows
 netsh interface portproxy add v4tov4 listenport=9092 listenaddress=0.0.0.0 connectport=9092 connectaddress=<your-ip>
 
+# Start Confluent
 confluent local services start
 
+# Create Topic
 kafka-topics --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic pos-topic
 
+# Read Kafka Topic
 kafka-console-consumer --bootstrap-server localhost:9092 --topic pos-topic --from-beginning --property print.key=true --property key.separator=":"
 
+# Stop Confluent
 confluent local services stop
+
+# Destroy Confluent
 confluent local destroy
