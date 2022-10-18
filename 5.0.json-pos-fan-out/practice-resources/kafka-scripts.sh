bf41463ce45e7cf065ad97a56ca18614263a7e56
Prateek <54174687+javaHelper@users.noreply.github.com>
2022-10-18 17:03:27 +0530

    Processing Json Message Stream!

--- 5.0.json-pos-fan-out/practice-resources/kafka-scripts.sh
+++ 5.0.json-pos-fan-out/practice-resources/kafka-scripts.sh
@@ -1,16 +1,22 @@
 
-
+# Below needed for windows only
 netsh interface portproxy add v4tov4 listenport=9092 listenaddress=0.0.0.0 connectport=9092 connectaddress=<your-ip>
 
 netsh interface portproxy add v4tov4 listenport=8081 listenaddress=0.0.0.0 connectport=8081 connectaddress=<your-ip>
 
+# Start Confluent
 confluent local services start
 
+# Create Topic
 kafka-topics --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic loyalty-topic
 kafka-topics --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic hadoop-sink-topic
 
+# Console Consumer
 kafka-avro-console-consumer --bootstrap-server localhost:9092 --topic loyalty-topic --from-beginning --property key.deserializer=org.apache.kafka.common.serialization.StringDeserializer --property print.key=true --property key.separator=":"
 kafka-avro-console-consumer --bootstrap-server localhost:9092 --topic hadoop-sink-topic --from-beginning --property key.deserializer=org.apache.kafka.common.serialization.StringDeserializer --property print.key=true --property key.separator=":"
 
+# Stop Confluent
 confluent local services stop
+
+3 Destroy Confluent
 confluent local destroy
